60ef45edb4b6f9ab3860949eae3a8bac7bc02a70
0anju0 <123807748+0anju0@users.noreply.github.com>
2023-05-17 20:49:53 +0530

    Update mongoose.js

--- config/mongoose.js
+++ config/mongoose.js
@@ -1,6 +1,6 @@
 const mongoose = require('mongoose');
 // mongoose.connect('mongodb://0.0.0.0/employeReviewSystem');
-const DB = 'mongodb://127.0.0.1/employeReviewSystem';
+const DB = 'mongodb+srv://anjubhakta2426:12345@cluster0.hmuvq0q.mongodb.net/?retryWrites=true&w=majority';
 
 // These set of line can be written in async await fashion, but I have followed the documentation. 
 mongoose.connect(DB).then(()=>{
