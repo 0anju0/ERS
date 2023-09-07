32c01fa5d4c3696ffc476c71b97c4e9c0eba686b
0anju0 <123807748+0anju0@users.noreply.github.com>
2023-09-07 21:20:59 +0530

    Update index.js

--- index.js
+++ index.js
@@ -44,7 +44,7 @@ app.use(session({
         maxAge: (1000 * 60 * 100)
     },
     store: MongoStore.create({
-        mongoUrl: 'mongodb+srv://whiteWolff:praduman@cluster0.an8uy3k.mongodb.net/ERS?retryWrites=true&w=majority',
+        mongoUrl: 'mongodb+srv://anjubhakta2426:12345@cluster0.hmuvq0q.mongodb.net/?retryWrites=true&w=majority',
         autoRemove: 'disabled'
     },
         (err) => {
